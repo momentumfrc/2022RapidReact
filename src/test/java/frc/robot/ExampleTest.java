57b5d3e93889d2eb5d19b1a33a72b4895f461b58
dssato <73856135+dssato@users.noreply.github.com>
2022-01-09 18:04:12 -0800

    Add JUnit test framework

---
+++ src/test/java/frc/robot/ExampleTest.java
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class that does nothing
+ */
+public class ExampleTest {
+    private static final Logger LOG = Logger.getLogger("4999 Rapid React/Test");
+
+    @Test
+    public void test() {
+        LOG.info("Beginning Tests");
+
+        assertEquals(2 + 2, 4);
+    }
+}
